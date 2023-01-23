f0991c57847bc2f3676a08dc5b877dcacca73d75
“Francesca <dascanio.francesca@gmail.com>
2023-01-23 17:22:36 +0100

    Completato esercizio

--- js/script.js
+++ js/script.js
@@ -27,6 +27,13 @@ const { createApp } = Vue
             }
         ],
         activeIndex: 0,
+        thumbs: [
+            'img/01.webp',
+            'img/02.webp',
+            'img/03.webp',
+            'img/04.webp',
+            'img/05.webp',
+        ],
 
       }
     },
